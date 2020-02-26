128336e10b8904da53669e55668ad84c6ed77b3d
Fernando Alarcon <erickfernando_20@hotmail.com>
2020-02-26 04:57:01 -0500

    best

--- app.js
+++ app.js
@@ -1,19 +1,22 @@
 const path = require('path');
 const express = require('express');
 const router = express.Router();
+const http = require('http');
 
 // INVOCACION DE LA BD
 const app = express();
 
 ////// SETTINGS
-app.set('port', process.env.PORT || 4040);
+const port = process.env.PORT || 4040;
 
 //// STATIC FILES
 app.use(express.static(path.join(__dirname, '/public')));
 
+let server = http.createServer(app);
 //////// START
-const server = app.listen(app.get('port'), () => {
-    console.log('Server on port', app.get('port'));
+server.listen(port, (err) => {
+    if (err) throw new Error(err);
+    console.log(`Servidor corriendo en puerto ${ port }`);
 });
 
 ////// WEBSOCKETS
